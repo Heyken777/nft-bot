f18135bc8017b21c90cc030bd44881f56563d429
Heyken777 <arkady20099@gmailcom>
2026-07-06 23:19:12 +0300

    fix media serving in production mode + conditional Войти/Выйти with distinct logout style

--- ProjectSite/novix_admin/urls.py
+++ ProjectSite/novix_admin/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as serve_static
+from django.urls import re_path
 from users import views
 from tickets import views as tickets_views
 from disputes import views as disputes_views
@@ -115,3 +117,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Всегда обслуживаем медиа, независимо от DEBUG
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve_static, {'document_root': settings.MEDIA_ROOT}),
+]
